54b25129edbb4468177327b4ae61e73224c92e3c
Conanan <54whn54@gmail.com>
2019-11-10 17:05:28 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,21 +11,19 @@ module.exports = {
   title: 'Conanan\'s Notes',
   description: 'Good Good Study, Day Day Up!',
   themeConfig: {
-    // 导航栏 logo
-    // logo: './logo.png',
+    
+    // logo: './logo.png', // 导航栏 logo
 
-    // 导航栏链接
+    displayAllHeaders: false, // 默认值：false，侧边栏打开显示所有页面的标题链接（同一目录下）
+    // sidebar: 'auto' // 自动生成侧边栏，仅包含了当前页面标题（headers）链接的侧边栏
+    lastUpdated: "上次更新时间",
+    
     nav: navConf,
-    
-    // 侧边栏打开显示所有页面的标题链接（同一目录下）
-    displayAllHeaders: false, // 默认值：false
-    
-    // 自动生成侧边栏，仅包含了当前页面标题（headers）链接的侧边栏
-    // sidebar: 'auto'
-
     sidebar: sidebarConf,
 
-    lastUpdated: "上次更新时间"
+
+    
+
   },
 
   markdown: markdownConf,
