cbe0e44dc140dce04d8a71df6d70d3e1eae71efd
Conanan <54whn54@gmail.com>
2019-11-12 00:05:25 +0800

    success

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,8 @@ npm run docs:build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+# 如果是发布到自定义域名，因为不是/notes/，所以 base 不需要配置，DNS 解析后，Github 也会解析
+echo 'notes.conanan.top' > CNAME
 
 git init
 git add -A
@@ -19,7 +19,7 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:apple54whn/apple54whn.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 如果发布到 https://<USERNAME>.github.io/<REPO>，此处与 base 配置不是一个意思！！！
 git push -f git@github.com:apple54whn/notes.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,13 +5,11 @@ const markdownConf = require('./config/markdownConf');
 // plugins
 const pluginConf = require('./config/pluginConf');
 
-
-
 module.exports = {
+  // base:'/notes/',
   title: 'Conanan\'s Notes',
   description: 'Good Good Study, Day Day Up!',
   themeConfig: {
-    base:'/notes/',
     smoothScroll: true,
     
     // logo: './logo.png', // 导航栏 logo
@@ -24,25 +22,25 @@ module.exports = {
     
     
     
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'apple54whn/notes',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    // repoLabel: '查看源码',
+    // // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    // repo: 'apple54whn/notes',
+    // // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    // // repoLabel: '查看源码',
     
-    // 以下为可选的编辑链接选项
+    // // 以下为可选的编辑链接选项
     
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    // docsRepo: 'vuejs/vuepress',
-    // 假如文档不是放在仓库的根目录下：
-    docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
-    // docsBranch: 'master',
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: '编辑文档！',
-    lastUpdated: "上次更新时间",
+    // // 假如你的文档仓库和项目本身不在一个仓库：
+    // // docsRepo: 'vuejs/vuepress',
+    // // 假如文档不是放在仓库的根目录下：
+    // docsDir: 'docs',
+    // // 假如文档放在一个特定的分支下：
+    // // docsBranch: 'master',
+    // // 默认是 false, 设置为 true 来启用
+    // editLinks: true,
+    // // 默认为 "Edit this page"
+    // editLinkText: '编辑文档！',
+    // lastUpdated: "上次更新时间",
     
   },
 
